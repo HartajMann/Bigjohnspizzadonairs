d7893a580ffc8c546653cb2d4e5ca1630c2ebace
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2024-02-01 23:25:25 -0700

    changes to timetracking part

--- Bigjohnspizzadonairs/Data/DbmaManager.cs
+++ Bigjohnspizzadonairs/Data/DbmaManager.cs
@@ -4,48 +4,93 @@ using System.Threading.Tasks;
 
 namespace Bigjohnspizzadonairs.Data
 {
-	public class DbmaManager
-	{
-		string connString;
-		SqlConnection sqlConnection;
+    public class DbmaManager
+    {
+        string connString;
 
-		// Constructor to initialize the variables and establish the database connection
-		public DbmaManager()
-		{
-			connString = @"Data Source=HARRY-PC\SQLEXPRESS;Initial Catalog=Bigjohnspizzadonairs;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
-			sqlConnection = new SqlConnection(connString);
-			sqlConnection.Open();
-		}
+        // Constructor to initialize the connection string
+        public DbmaManager()
+        {
+            connString = @"Data Source=HARRY-PC\SQLEXPRESS;Initial Catalog=Bigjohnspizzadonairs;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+        }
 
         public async Task<bool> ValidateLoginAsync(string userId, string password)
         {
-            using (SqlCommand command = new SqlCommand("SELECT Password FROM Login WHERE UserId = @UserId", sqlConnection))
+            using (var connection = new SqlConnection(connString))
             {
+                await connection.OpenAsync();
+                var query = "SELECT Password FROM Login WHERE UserId = @UserId";
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    var result = await command.ExecuteScalarAsync();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        string storedPassword = (string)result;
+                        return storedPassword == password;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public async Task<bool> PunchInAsync(string userId)
+        {
+            using (var sqlConnection = new SqlConnection(connString))
+            {
+                await sqlConnection.OpenAsync();
+
+                var command = new SqlCommand("INSERT INTO TimeTracking (UserId, PunchIn) VALUES (@UserId, GETDATE())", sqlConnection);
                 command.Parameters.AddWithValue("@UserId", userId);
 
-                // Open the connection just before executing the command
-                if (sqlConnection.State != System.Data.ConnectionState.Open)
-                    await sqlConnection.OpenAsync();
+                int result = await command.ExecuteNonQueryAsync();
 
-                object result = await command.ExecuteScalarAsync();
-
-                // Close the connection after executing the command
                 await sqlConnection.CloseAsync();
 
-                if (result != null && result != DBNull.Value)
-                {
-                    string storedPassword = (string)result;
-                    return storedPassword == password;
-                }
-                else
-                {
-                    return false;
-                }
+                return result > 0;
             }
         }
 
+        public async Task<bool> PunchOutAsync(string userId)
+        {
+            using (var sqlConnection = new SqlConnection(connString))
+            {
+                await sqlConnection.OpenAsync();
 
+                var command = new SqlCommand("UPDATE TimeTracking SET PunchOut = GETDATE() WHERE UserId = @UserId AND PunchOut IS NULL", sqlConnection);
+                command.Parameters.AddWithValue("@UserId", userId);
 
+                int result = await command.ExecuteNonQueryAsync();
+
+                await sqlConnection.CloseAsync();
+
+                return result > 0;
+            }
+        }
+
+        public async Task<(DateTime? punchIn, DateTime? punchOut)> GetLastActivityAsync(string userId)
+        {
+            using (var sqlConnection = new SqlConnection(connString))
+            {
+                await sqlConnection.OpenAsync();
+
+                var command = new SqlCommand("SELECT TOP 1 PunchIn, PunchOut FROM TimeTracking WHERE UserId = @UserId ORDER BY TrackingId DESC", sqlConnection);
+                command.Parameters.AddWithValue("@UserId", userId);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        var punchIn = reader["PunchIn"] as DateTime?;
+                        var punchOut = reader["PunchOut"] as DateTime?;
+                        return (punchIn, punchOut);
+                    }
+                }
+
+                await sqlConnection.CloseAsync();
+                return (null, null);
+            }
+        }
 
     }
 }
